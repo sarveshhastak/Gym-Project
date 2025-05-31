35399478083590032394dfd1d23920014dbbe10e
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-05-31 16:02:25 +0530

    updated frontend and backend

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 const cookieParser = require("cookie-parser")
 const { adminProtected, customerProtected } = require("./middleware/auth.middleware")
 require("dotenv").config()
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+const path = require("path");
+app.use(express.static("dist"));
+app.use(cors({ origin: "https://gym-project-uwzk.onrender.com", credentials: true }))
 app.use(cookieParser())
 app.use("/api/auth", require("./routes/auth.route"))
 app.use("/api/product", adminProtected, require("./routes/admin.route"))
 app.use("/api/public", require("./routes/public.routes"))
 app.use("/api/customer", customerProtected, require("./routes/customer.route"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found !" })
-})
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.once("open", () => {
     console.log("Mongodb Connected...")
