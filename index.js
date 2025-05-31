b6e89cb07b1ba99e02836bd9cd9c2ec3eaafa02f
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-05-31 16:05:46 +0530

    final updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const path = require("path")
 const cookieParser = require("cookie-parser")
 const { adminProtected, customerProtected } = require("./middleware/auth.middleware")
 require("dotenv").config()
