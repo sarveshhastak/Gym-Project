ff26e021fe82229c06611d0dde2016a28a7d7470
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-09-25 15:51:30 +0530

    updated

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.json())
 const path = require("path");
 app.use(express.static("dist"));
-app.use(cors({ origin: "https://gym-project-uwzk.onrender.com", credentials: true }))
+// app.use(cors({ origin: "https://gym-project-uwzk.onrender.com", credentials: true }))
+app.use(cors({ origin: "https://gym-project-1-hcva.onrender.com", credentials: true }))
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use(cookieParser())
 app.use("/api/auth", require("./routes/auth.route"))
